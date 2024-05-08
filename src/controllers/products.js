bcb3f11a4001f40e589f8ae685dd26536c484ee7
Guilherme <guilherme.reis@tripservice.com.br>
2024-05-08 16:34:35 -0300

    Adicionando metodo post

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,12 +1,37 @@
 const ProductsModel = require('../models/products')
 
 async function get(req, res) {
+    const { id } = req.params
 
-    const products = await ProductsModel.find()
+    const obj = id ? { _id: id } : null
+
+    const products = await ProductsModel.find(obj)
 
     res.send(products)
 }
 
+async function post(req, res) {
+    const {
+        name,
+        brand,
+        price,
+    } = req.body
+
+    const product = new ProductsModel({
+        name,
+        brand,
+        price,
+    })
+
+    product.save()
+
+
+    res.send({
+        message: 'sucess!'
+    })
+}
+
 module.exports = {
     get,
+    post,
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,8 +10,8 @@ DELETE - remover dados
 
 const ProductController = require('../controllers/products')
 
-router.get('/products', ProductController.get)
-// router.post('/products', ProductController.post)
+router.get('/products/:id?', ProductController.get)
+router.post('/products', ProductController.post)
 // router.put('/products/:id', ProductController.put)
 // router.delete('/products/:id', ProductController.delete)
 
